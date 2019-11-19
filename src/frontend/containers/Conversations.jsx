569458b20c84f8733f839503e6025ee761f519be
Tonalli Lopez <aguilablanca_7@live.com>
2019-11-19 16:42:08 -0600

    empezando con las ultimas dos pantallas

---
+++ src/frontend/containers/Conversations.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Container,
+} from '../assets/styles/Conversation';
+
+import Chats from '../components/Chats';
+
+const Conversation = () => (
+  <Container>
+    <Chats />
+  </Container>
+);
+
+export default Conversation;
