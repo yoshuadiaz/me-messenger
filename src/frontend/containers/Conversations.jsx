88171cb8ccead6483b7a37ba5a01f0b6b1151250
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2019-11-20 17:49:59 -0600

    feature/loadChats

--- src/frontend/containers/Conversations.jsx
+++ src/frontend/containers/Conversations.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
   Container,
 } from '../assets/styles/Conversation';
 
 import Chats from '../components/Chats';
 
-const Conversation = () => (
-  <Container>
-    <Chats />
-  </Container>
-);
+const Conversation = (props) => {
+  const { conversations } = props;
+  return (
+    <Container>
+      <Chats conversations={conversations} />
+    </Container>
+  );
+};
 
-export default Conversation;
+const mapStateToProps = (state) => ({
+  conversations: state.conversations,
+});
+
+export default connect(mapStateToProps, null)(Conversation);
