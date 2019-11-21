e902918e58edc5983a4fedfd04bc0d7a1d2b9658
Tonalli Lopez <aguilablanca_7@live.com>
2019-11-21 13:30:58 -0600

    terminando la pantalla de Chats con la opción de crear uno nuevo

---
+++ src/frontend/assets/styles/Conversations.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  grid-area: content;
+  display: flex;
+  max-height: 100vh;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  h1{
+    font-size: 35px;
+    font-weight: 700;
+  }
+`;
--- src/frontend/containers/Conversations.jsx
+++ src/frontend/containers/Conversations.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { Container } from '../assets/styles/Conversations';
+import { Buttonm } from '../assets/styles/GlobalStyles';
+
 const Conversation = () => (
-  <h1>Conversaciones</h1>
+  <Container>
+    <h1>Aún no has iniciado ninguna conversación</h1>
+    <Buttonm>Iniciar una nueva conversación</Buttonm>
+  </Container>
 );
 
 export default Conversation;
