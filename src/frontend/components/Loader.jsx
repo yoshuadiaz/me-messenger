cd30e883bae1174ca5772e53ca77f43e101384f6
Tonalli Lopez <aguilablanca_7@live.com>
2019-11-22 20:34:52 -0600

    creando un Loader

---
+++ src/frontend/components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Loader as Container } from '../assets/styles/Loader';
+
+const Loader = () => (
+  <Container>
+    <div />
+    <div />
+    <div />
+    <div />
+  </Container>
+);
+
+export default Loader;
